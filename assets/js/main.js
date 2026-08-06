2ecd93cf3ea31e7749915a59667f6939cf34bea8
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-08-06 16:06:46 +0300

    add delete logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,10 @@ const category = document.querySelector("#category");
 const createbtn = document.querySelector("#createbtn");
 const totalContainer = document.querySelector("#totalContainer");
 const productsTable = document.querySelector("#productsTable");
-const equalSign = document.querySelector(".equalSign");
+const seperator = document.querySelector(".seperator");
+const deleteAllbtn = document.querySelector("#deleteAllbtn");
+const dataSection = document.querySelector(".outputs");
+
 if (localStorage.getItem("theme") == "dark") {
   html.classList.add("dark");
   modeicon.classList.replace("fa-moon", "fa-sun");
@@ -37,13 +40,9 @@ const getTotal = () => {
     total.innerHTML = result;
     totalContainer.classList.remove("bg-rose-400/70", "dark:bg-rose-500/40");
     totalContainer.classList.add("bg-emerald-400/70", "dark:bg-emerald-500/40");
-    equalSign.classList.remove("hidden");
+    seperator.classList.remove("hidden");
   } else {
-    console.log("in");
     total.innerHTML = "";
-    totalContainer.classList.remove("bg-rose-400/70", "dark:bg-rose-500/40");
-    totalContainer.classList.add("bg-emerald-400/70", "dark:bg-emerald-500/40");
-    equalSign.classList.add("hidden");
   }
 };
 
@@ -67,11 +66,24 @@ createbtn.onclick = (e) => {
     count: count.value,
     category: category.value,
   };
-  products.push(newProduct);
+
+  if (newProduct.count > 1) {
+    console.log(newProduct.count);
+    for (let i = 0; i < newProduct.count; i++) {
+    products.push(newProduct);
+    }
+  } else{
+    products.push(newProduct);
+  }
+  seperator.classList.add("hidden");
+  totalContainer.classList.remove(
+    "bg-emerald-400/70",
+    "dark:bg-emerald-500/40",
+  );
+  totalContainer.classList.add("bg-rose-400/70", "dark:bg-rose-500/40");
   saveToLocalStorage();
   clearData();
   showData();
-  console.log(products);
 };
 
 // save to local storage
@@ -93,20 +105,27 @@ const clearData = () => {
 
 // read
 const showData = () => {
-  let table = '';
-  products
-    .map(
-      (product) =>
-        (table += `
+  if (products.length > 0) {
+    deleteAllbtn.innerHTML = `<button type="button" onclick="deleteAll()"
+              class="w-full p-3 bg-purple-400 text-black dark:text-white dark:bg-purple-500 hover:bg-purple-500 dark:hover:bg-purple-400 font-semibold hover:tracking-wider rounded-2xl transition-colors duration-200 ease-in-out">
+              Delete All (${products.length})</button>`;
+    dataSection.classList.remove("hidden");
+  } else {
+    deleteAllbtn.innerHTML = "";
+    dataSection.classList.add("hidden");
+  }
+  let table = "";
+  for (let i = 0; i < products.length; i++) {
+    table += `
         <tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-gray-800 dark:even:bg-gray-700">
-          <td class="p-3 text-sm">${+product.id++}</td>
-          <td class="p-3">${product.title}</td>
-          <td class="p-3">${product.taxes}</td>
-          <td class="p-3">${product.ads}</td>
-          <td class="p-3 font-semibold">${product.discount}</td>
-          <td class="p-3">${product.total}</td>
-          <td class="p-3 font-semibold">${product.category}</td>
-          <td class="p-3">${product.price}</td>
+          <td class="p-3 text-sm">${i + 1}</td>
+          <td class="p-3">${products[i].title}</td>
+          <td class="p-3">${products[i].price}</td>
+          <td class="p-3">${products[i].taxes}</td>
+          <td class="p-3">${products[i].ads}</td>
+          <td class="p-3 font-semibold">${products[i].discount}</td>
+          <td class="p-3 font-semibold">${products[i].category}</td>
+          <td class="p-3">${products[i].total}</td>
           <td class="p-3">
             <button
               class="px-2 py-1 bg-purple-300 dark:bg-purple-500 text-white rounded-md hover:bg-purple-400 dark:hover:bg-purple-600 transition-colors">
@@ -115,23 +134,34 @@ const showData = () => {
                   d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z" />
               </svg>
             </button>
-            <button class="px-2 py-1 bg-red-500 text-white rounded-md ml-2 hover:bg-red-600 transition-colors">
+            <button onclick="deleteItem(${i})" class="px-2 py-1 bg-red-500 text-white rounded-md ml-2 hover:bg-red-600 transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path fill="currentColor"
                   d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3.713-4.288Q11 16.426 11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17t.713-.288m4 0Q15 16.426 15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17t.713-.288" />
               </svg>
             </button>
           </td>
-      </tr>
-  `),
-    )
-    .join("");
-
+      </tr>`;
+  }
   productsTable.innerHTML = table;
 };
-// count
+
 // delete
+const deleteItem = (id) => {
+  products.splice(id, 1);
+  saveToLocalStorage();
+  showData();
+};
+
+// delete all
+const deleteAll = () => {
+  products = [];
+  saveToLocalStorage();
+  showData();
+};
+
 // update
+
 // search
 // clean data
 
