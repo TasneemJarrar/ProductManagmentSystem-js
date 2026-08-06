1d48ebfdba382d4644ce027b88f6ce4a8fb7ac00
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-08-06 14:49:18 +0300

    add showing products table logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
 const modebtn = document.getElementById("mode-toggle");
 const modeicon = document.getElementById("mode-icon");
 const html = document.documentElement;
-let title = document.querySelector("#title");
-let price = document.querySelector("#price");
-let taxes = document.querySelector("#taxes");
-let ads = document.querySelector("#ads");
-let discount = document.querySelector("#discount");
-let total = document.querySelector("#total");
-let count = document.querySelector("#count");
-let category = document.querySelector("#category");
-let createbtn = document.querySelector("#createbtn");
-let totalContainer = document.querySelector("#totalContainer");
-
+const title = document.querySelector("#title");
+const price = document.querySelector("#price");
+const taxes = document.querySelector("#taxes");
+const ads = document.querySelector("#ads");
+const discount = document.querySelector("#discount");
+const total = document.querySelector("#total");
+const count = document.querySelector("#count");
+const category = document.querySelector("#category");
+const createbtn = document.querySelector("#createbtn");
+const totalContainer = document.querySelector("#totalContainer");
+const productsTable = document.querySelector("#productsTable");
+const equalSign = document.querySelector(".equalSign");
 if (localStorage.getItem("theme") == "dark") {
   html.classList.add("dark");
   modeicon.classList.replace("fa-moon", "fa-sun");
@@ -33,15 +34,18 @@ modebtn.addEventListener("click", () => {
 const getTotal = () => {
   if (price.value && taxes.value && ads.value) {
     let result = +price.value + +taxes.value + +ads.value - +discount.value;
-    total.innerHTML = ": " + result;
+    total.innerHTML = result;
     totalContainer.classList.remove("bg-rose-400/70", "dark:bg-rose-500/40");
     totalContainer.classList.add("bg-emerald-400/70", "dark:bg-emerald-500/40");
+    equalSign.classList.remove("hidden");
   } else {
+    console.log("in");
     total.innerHTML = "";
-    totalContainer.classList.add("bg-rose-400/70", "dark:bg-rose-500/40");
+    totalContainer.classList.remove("bg-rose-400/70", "dark:bg-rose-500/40");
+    totalContainer.classList.add("bg-emerald-400/70", "dark:bg-emerald-500/40");
+    equalSign.classList.add("hidden");
   }
 };
-
 
 let products;
 if (localStorage.getItem("products") === null) products = [];
@@ -53,6 +57,7 @@ else {
 createbtn.onclick = (e) => {
   e.preventDefault();
   let newProduct = {
+    id: 1,
     title: title.value,
     price: price.value,
     taxes: taxes.value,
@@ -65,6 +70,7 @@ createbtn.onclick = (e) => {
   products.push(newProduct);
   saveToLocalStorage();
   clearData();
+  showData();
   console.log(products);
 };
 
@@ -74,20 +80,59 @@ const saveToLocalStorage = () => {
 };
 
 // clear inputs
-const clearData = () =>{
-  title.value="";
-  price.value="";
-  taxes.value="";
-  ads.value="";
-  discount.value="";
-  total.innerHTML="";
-  count.value="";
-  category.value="";
-}
+const clearData = () => {
+  title.value = "";
+  price.value = "";
+  taxes.value = "";
+  ads.value = "";
+  discount.value = "";
+  total.innerHTML = "";
+  count.value = "";
+  category.value = "";
+};
 
 // read
+const showData = () => {
+  let table = '';
+  products
+    .map(
+      (product) =>
+        (table += `
+        <tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-gray-800 dark:even:bg-gray-700">
+          <td class="p-3 text-sm">${+product.id++}</td>
+          <td class="p-3">${product.title}</td>
+          <td class="p-3">${product.taxes}</td>
+          <td class="p-3">${product.ads}</td>
+          <td class="p-3 font-semibold">${product.discount}</td>
+          <td class="p-3">${product.total}</td>
+          <td class="p-3 font-semibold">${product.category}</td>
+          <td class="p-3">${product.price}</td>
+          <td class="p-3">
+            <button
+              class="px-2 py-1 bg-purple-300 dark:bg-purple-500 text-white rounded-md hover:bg-purple-400 dark:hover:bg-purple-600 transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                  d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z" />
+              </svg>
+            </button>
+            <button class="px-2 py-1 bg-red-500 text-white rounded-md ml-2 hover:bg-red-600 transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                  d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3.713-4.288Q11 16.426 11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17t.713-.288m4 0Q15 16.426 15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17t.713-.288" />
+              </svg>
+            </button>
+          </td>
+      </tr>
+  `),
+    )
+    .join("");
+
+  productsTable.innerHTML = table;
+};
 // count
 // delete
 // update
 // search
 // clean data
+
+showData();
